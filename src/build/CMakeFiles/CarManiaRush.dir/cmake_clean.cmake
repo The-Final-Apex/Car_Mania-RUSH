9ee3e5ad605286ad5317eab4d7c831e6e15114b1
The-Final-Apex <164732339+The-Final-Apex@users.noreply.github.com>
2025-11-27 14:01:27 +0300

    added a sample build
    :3

---
+++ src/build/CMakeFiles/CarManiaRush.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CarManiaRush.dir/main.cpp.o"
+  "CMakeFiles/CarManiaRush.dir/main.cpp.o.d"
+  "CarManiaRush"
+  "CarManiaRush.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CarManiaRush.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
